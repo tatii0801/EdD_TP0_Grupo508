c0e27fc781c04f20e9345685923163d09536b087
Romi:)Santos <42072474@fi.unju.edu.ar>
2026-09-07 22:47:09 -0300

    Agrego desarrollo del ejercico 4

---
+++ src/main/java/tp4/Ticket.java
@@ -0,0 +1,31 @@
+package tp4;
+
+public class Ticket {
+    
+    private int idTicket;
+    private String departamento;
+    private int nivelUrgencia;
+
+    public Ticket(int idTicket, String departamento, int nivelUrgencia) {
+        this.idTicket = idTicket;
+        this.departamento = departamento;
+        this.nivelUrgencia = nivelUrgencia;
+    }
+
+    public int getIdTicket() {
+        return idTicket;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public int getNivelUrgencia() {
+        return nivelUrgencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket ID: " + idTicket + " | Área: " + departamento + " | Urgencia: " + nivelUrgencia;
+    }
+}
---
+++ target/classes/tp4/README.md
@@ -0,0 +1,12 @@
+# Trabajo Práctico N° 4 - Colas (Queue)
+Resolución de los ejercicios del TP.
+
+D I S T R I B U C I Ó N :
+Ejercicio 1 resuelto por:
+Ejercicio 2 resuelto por:
+Ejercicio 3 resuelto por:
+Ejercicio 4 resuelto por: Romina Santos.
+Ejercicio 5 resuelto por:
+Ejercicio 6 resuelto por:
+Ejercicio 7 resuelto por:
+Ejercicio 8 resuelto por:
